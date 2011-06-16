69da8ea2f3b7a304651e03a4146a47c0de0833ea
Serge Balyuk <bgipsy@gmail.com>
2011-06-16 07:42:03 +0300

    style fix

--- lib/model_factory/single_model_association.rb
+++ lib/model_factory/single_model_association.rb
@@ -6,15 +6,15 @@ class ModelFactory::SingleModelAssociation < ModelFactory::Member
   
   def to_value
     if @block
-      ModelFactory::DSL::AssociationBlock.new(@reflection.klass, :allow_use => allow_use).run(&@block).model
+      ModelFactory::DSL::AssociationBlock.new(@reflection.klass, :allow_use => allow_use?).run(&@block).model
     elsif @value.is_a?(Symbol)
-      allow_use ? @reflection.klass.factory.use(@value) : @reflection.klass.factory.create(@value)
+      allow_use? ? @reflection.klass.factory.use(@value) : @reflection.klass.factory.create(@value)
     else
       @value
     end
   end
   
-  def allow_use
+  def allow_use?
     @reflection.macro != :has_one
   end
   
